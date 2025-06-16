87985525f0882ea672ab6dfdce8192c19db2b69b
Diego Costa <diego@diegocosta.me>
2025-06-16 18:46:42 -0300

    finished logo effect

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,26 +1,26 @@
-'use client';
+"use client";
 
-import { type PropsWithChildren, useState } from 'react';
-import classes from './styles.module.css';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { AiOutlineClose } from 'react-icons/ai';
-import Logo from '@/public/assets/LOGO.png';
-import Image from 'next/image';
+import { type PropsWithChildren, useState } from "react";
+import classes from "./styles.module.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import Logo from "@/public/assets/LOGO.png";
+import Image from "next/image";
 
-export default function NavBar({children} : PropsWithChildren){
-    const [menuOpen, setMenuOpen] = useState(false);
-    return(
-        <div className={classes.container}>
-            <button 
-            className={classes.hamburguesa} 
-            onClick={()=> setMenuOpen(!menuOpen)}
-            >
-                {menuOpen ? <AiOutlineClose/> : <GiHamburgerMenu/>}
-            </button>
-            <nav data-mobile-open={menuOpen} className={classes.navBar}>
-                 <Image className={classes.logo} src={Logo} alt=""/>
-                { children } 
-            </nav>
-        </div>
-    );
+export default function NavBar({ children }: PropsWithChildren) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div className={classes.container}>
+      <button
+        className={classes.hamburguesa}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+      </button>
+      <nav data-mobile-open={menuOpen} className={classes.navBar}>
+        <Image className={classes.logo} src={Logo} alt="" />
+        {children}
+      </nav>
+    </div>
+  );
 }
